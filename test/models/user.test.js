bc082d497a816c478aad1056c21178ea4964efa1
AlexMaderaP <alejandro.maderap@gmail.com>
2022-04-21 12:24:18 -0500

    Agregando requerimento 2 TDD

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -7,14 +7,12 @@ describe("Unit Test for User Class", () => {
       "AlexMadera",
       "Alejandro Madera",
       "Bio",
-      "2/02/2022",
-      "4/20/2022"
     );
     expect(alex.id).toBe(1);
     expect(alex.username).toBe("AlexMadera");
     expect(alex.name).toBe("Alejandro Madera");
     expect(alex.bio).toBe("Bio");
-    expect(alex.dateCreated).toBe("2/02/2022");
-    expect(alex.lastUpdated).toBe("4/20/2022");
+    expect(alex.dateCreated).not.toBeUndefined();
+    expect(alex.lastUpdated).not.toBeUndefined();
   })
 })
